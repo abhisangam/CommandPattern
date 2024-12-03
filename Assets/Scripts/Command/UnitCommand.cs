2b6f153d885afe2636374a06aea30c483ea1fd2b
Abhishek Sangam <abhishek.sangam@harman.com>
2024-12-03 22:25:17 +0530

    Added abstract commands

---
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -0,0 +1,19 @@
+﻿using Command.Player;
+using System.Collections;
+using UnityEngine;
+
+
+public abstract class UnitCommand : ICommand
+{
+    public int ActorUnitID;
+    public int TargetUnitID;
+    public int ActorPlayerID;
+    public int TargetPlayerID;
+
+    protected UnitController actorUnit;
+    protected UnitController targetUnit;
+
+    public abstract void Execute();
+
+    public abstract bool WillHitTarget();
+}
