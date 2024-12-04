6ce85312ea471fe92c9e96cf06a34c11601f88f1
Abhishek Sangam <abhishek.sangam@harman.com>
2024-12-04 10:58:38 +0530

    Added command invoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CommandInvoker
+{
+    private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+
+    public void ProcessCommand(ICommand connamdToProcess)
+    {
+        ExecuteCommand(connamdToProcess);
+        RegisterCommand(connamdToProcess);
+    }
+
+    public void ExecuteCommand(ICommand command)
+    {
+        command.Execute();
+    }
+
+    public void RegisterCommand(ICommand command) { commandRegistry.Push(command); }
+}
